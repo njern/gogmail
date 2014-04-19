fe75b517be068d40c357bf7109fb4a6829268bb9
Niclas Jern <niclas@walkbase.com>
2014-04-19 12:19:48 +0300

    Add support for HTML email.

--- gogmail.go
+++ gogmail.go
@@ -25,13 +25,19 @@ func GmailConnection(username, password string) *GMail {
 }
 
 // SendMail() sends a mail via GMail - assuming you have valid credentials and a working internet connection.
-func (gmail *GMail) SendMail(recipients []string, subject, body string) error {
+func (gmail *GMail) SendMail(recipients []string, subject, body string, isHTML bool) error {
 	// Remove any newlines before the body...
 	sender := strings.Replace(gmail.username, "\n", "", -1)
 	recipientString := strings.Replace(strings.Join(recipients, ", "), "\n", "", -1)
 	subject = strings.Replace(subject, "\n", "", -1)
 
-	message := fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\n%s", sender, recipientString, subject, body)
+	var message string
+	if isHTML {
+		mime := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+		message = fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\n%s%s", sender, recipientString, subject, mime, body)
+	} else {
+		message = fmt.Sprintf("From: %s\nTo: %s\nSubject: %s\n\n%s", sender, recipientString, subject, body)
+	}
 
 	err := smtp.SendMail(fmt.Sprintf("%s:%d", SERVER, PORT),
 		*gmail.auth,
